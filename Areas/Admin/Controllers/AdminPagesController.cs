8767a2e644ca7c5cb13a0786da14ac0a3b5dd6fc
Fredrick Ondieki <FOndieki@my.gcu.edu>
2019-11-11 06:07:18 -0600

    Customized the Admin dashboard to include creatining new pages

--- Areas/Admin/Controllers/AdminPagesController.cs
+++ Areas/Admin/Controllers/AdminPagesController.cs
@@ -29,9 +29,67 @@ namespace Online_Shopping.Areas.Admin.Controllers
         }
         
         //Get admin pages and retun a view
+        [HttpGet]
         public ActionResult CreatePage()
         {
             return View();
+        }
+
+        //Post admin pages and retun a view
+        [HttpPost]
+        public ActionResult CreatePage(AdminPagesVm model)
+        {
+            //Validaate the model state
+            if (! ModelState.IsValid)
+            {
+                return View(model);
+
+            }
+
+            using (MyDb db = new MyDb())
+            {
+
+
+                //Declare the Slug
+                string slug;
+
+                //Initialize the PageDTO
+                PageDTO dTO = new PageDTO();
+               
+                //Display the DTO Title
+                dTO.Title = model.Title;
+
+                //Check and Set the Slug
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", " ").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", " ").ToLower();
+                }
+                //Make sure the Title and Slug are Unique from each other
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError(" ", "The Title or Slug entered already exist. ");
+                    return View(model);
+                }
+                //DTO the rest of the pages
+
+                dTO.Slug = slug;
+                dTO.Body = model.Body;
+                dTO.HasSidebar = model.HasSidebar;
+                dTO.Sorting = 100;
+
+                //Save the pageDTO
+                db.Pages.Add(dTO);
+                db.SaveChanges();
+            }
+            //Save the temporary message
+
+            TempData["Success"] = "You have successfully added a new page!";
+            //Redirect the page
+            return RedirectToAction("CreatePage");
         }
     }
 }
