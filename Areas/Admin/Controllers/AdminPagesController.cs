d0179fecbad42401a352b12c57062c540f308fa7
Fredrick Ondieki <FOndieki@my.gcu.edu>
2019-11-11 08:18:27 -0600

    Performed the edit functionality of the edit pages on the Admin dash board

--- Areas/Admin/Controllers/AdminPagesController.cs
+++ Areas/Admin/Controllers/AdminPagesController.cs
@@ -62,7 +62,7 @@ namespace Online_Shopping.Areas.Admin.Controllers
                 //Check and Set the Slug
                 if (string.IsNullOrWhiteSpace(model.Slug))
                 {
-                    slug = model.Title.Replace(" ", " ").ToLower();
+                    slug = model.Title.Replace(" ", "-").ToLower();
                 }
                 else
                 {
@@ -93,6 +93,7 @@ namespace Online_Shopping.Areas.Admin.Controllers
         }
 
         //Edit Admin Pages (Edit Functionality).
+        [HttpGet]
         public ActionResult EditPage(int id)
         {
             //Declare the page view model
@@ -114,6 +115,57 @@ namespace Online_Shopping.Areas.Admin.Controllers
             }
             //Return the view with the model
             return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditPage(AdminPagesVm model)
+        {
+            //Check model State
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+
+            }
+            using (MyDb db = new MyDb())
+            {
+                //Get the Page Id
+                int id = model.Id;
+                //Delcare Slug
+                string slug ="home";
+                //Get the Page
+                PageDTO dTO = db.Pages.Find(id);
+                //Reference DTO to the Page Title
+                dTO.Title = model.Title;
+                //Check for the Slug on the page and set it if there is need
+                if (model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+                //Make sure the Title and Slug are unique
+                if (db.Pages.Where(x => x.Id !=id).Any(x => x.Title == model.Title) ||
+                    db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError(" ", "The Slug or Title requested already exists!");
+                    return View(model);
+                }
+                //DTO the rest of the Pages
+                dTO.Slug = slug;
+                dTO.Body = model.Body;
+                dTO.HasSidebar = model.HasSidebar;
+                //Save the DTO
+                db.SaveChanges();
+            }
+            //Set Temp data message
+            TempData["SM"] = "You have successfully Edited the page!";
+            //Redirect the pages
+            return RedirectToAction("EditPage");
         }
     }
 
